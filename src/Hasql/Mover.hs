cfa8d173cd34b84758196d5f6788d38f578cf62d
Mike Ledger <eleventynine@gmail.com>
2024-04-21 20:41:22 +1000

    cli

--- src/Hasql/Mover.hs
+++ src/Hasql/Mover.hs
@@ -8,24 +8,27 @@ module Hasql.Mover (
   Rollback (..),
 
   -- * Checking and running migrations
-  CheckedMigrations,
-  checkMigrations,
-  runPendingMigrations,
+  performMigrations,
 ) where
 
 import Control.Monad (forM_, void)
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except (ExceptT (..), runExceptT, withExceptT)
 import Control.Monad.Trans.State.Strict (StateT (..), execStateT, gets, modify')
+import Data.ByteString (ByteString)
 import Data.Char (isSpace)
 import Data.Proxy (Proxy (..))
 import Data.SOP.Constraint (All)
 import Data.SOP.NP (NP (..), cpure_NP, traverse__NP)
+import Data.String (fromString)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.Encoding qualified as Text
 import Data.Time (UTCTime)
 import Data.Typeable (Typeable, cast)
 import Data.Void (Void)
+import Hasql.Connection qualified as Sql
 import Hasql.Session qualified as Sql
 import Hasql.TH qualified as Sql
 import Hasql.Transaction qualified as Tx
@@ -33,6 +36,10 @@ import Hasql.Transaction.Sessions qualified as Tx
 import Language.Haskell.TH qualified as TH
 import Language.Haskell.TH.Quote qualified as TH
 import Language.Haskell.TH.Syntax qualified as TH
+import Options.Applicative qualified as O
+import Prettyprinter ((<+>))
+import Prettyprinter qualified as R
+import Prettyprinter.Render.Terminal (Color (..), color, colorDull, putDoc)
 import PyF (strTrim)
 import Text.Megaparsec qualified as M
 import Text.Megaparsec.Char qualified as M
@@ -163,21 +170,84 @@ checkMigrations =
     addUp executedAt =
       modify' \CheckState {..} -> CheckState {ups = UpMigration {migration = migration @m, ..} : ups, ..}
 
-runPendingMigrations :: CheckedMigrations names -> Sql.Session ()
-runPendingMigrations CheckedMigrations {pendings} = do
-  forM_ pendings \(PendingMigration (m :: m)) -> do
-    case cast m of
-      Just BaseMigration -> Tx.transaction Tx.Serializable Tx.Write $ Tx.sql $ Text.encodeUtf8 $ up @BaseMigration
-      _ -> pure ()
-    _now <-
-      Tx.transaction Tx.Serializable Tx.Write $
-        Tx.statement
-          (migrationName @m, up @m, down @m)
-          [Sql.singletonStatement|
-        INSERT INTO hasql_mover_migration (name, up, down) VALUES($1::text, $2::text, $3::text)
-        RETURNING time::timestamptz
-      |]
-    pure ()
+--------------------------------------------------------------------------------
+-- Performing migrations
+
+data MigrationCli = MigrationCli
+  { connect :: IO (Either Sql.ConnectionError Sql.Connection)
+  , cmd :: MigrationCmd
+  }
+
+data MigrationCmd
+  = MigrateUp
+  | MigrateDown {useNewDownDefinition :: Bool}
+  | MigrateRedo
+  | MigrateStatus
+
+migrationCli :: O.Parser MigrationCli
+migrationCli =
+  MigrationCli
+    <$> (Sql.acquire . Text.encodeUtf8 . Text.pack <$> O.strOption (O.long "db" <> O.metavar "DB"))
+    <*> O.subparser
+      ( mconcat
+          [ O.command "up" (O.info migrateUp (O.progDesc "Perform any pending migrations"))
+          , O.command "down" (O.info migrateDown (O.progDesc "Rollback the last migration"))
+          , O.command "redo" (O.info migrateDown (O.progDesc "Redo the last migration"))
+          , O.command "status" (O.info migrateDown (O.progDesc "Check current status"))
+          ]
+      )
+  where
+    migrateUp, migrateDown, migrateRedo, migrateStatus :: O.Parser MigrationCmd
+    migrateUp = pure MigrateUp
+    migrateDown = MigrateDown <$> O.switch (O.long "useNewDownDefinition" <> O.help "Use the definition of 'down' defined in code rather than stored currently in the migration table in database")
+    migrateRedo = pure MigrateRedo
+    migrateStatus = pure MigrateStatus
+
+data MigrationError
+  = MigrationCheckError !Sql.QueryError
+  | MigrationConnectError !Sql.ConnectionError
+  deriving stock (Show)
+
+performMigrations
+  :: forall migrations
+   . (All Migration migrations)
+  => MigrationCli
+  -> IO (Either MigrationError ())
+performMigrations MigrationCli {connect, cmd} = runExceptT do
+  db <- errBy MigrationConnectError connect
+  CheckedMigrations {ups, divergents, pendings} <- errBy MigrationCheckError (Sql.run (checkMigrations @migrations) db)
+  case cmd of
+    MigrateStatus -> do
+      liftIO . putDoc . R.vsep $
+        [ foldMap ppUp ups
+        , foldMap ppDivergent divergents
+        , foldMap ppPending pendings
+        ]
+    _ -> pure ()
+  where
+    ppUp UpMigration {migration, executedAt} =
+      R.annotate (color Green) $ R.hsep ["[ UP ", R.viaShow executedAt, " ]", R.align (R.viaShow migration)]
+    ppDivergent DivergentMigration {migration, executedAt} =
+      R.annotate (color Red) $ R.hsep ["[ DIVERGENT ", R.viaShow executedAt, " ]", R.align (R.viaShow migration)]
+    ppPending PendingMigration {migration} =
+      R.annotate (colorDull White) $ R.hsep ["[ PENDING ]", R.align (R.viaShow migration)]
+
+    errBy f a = withExceptT f (ExceptT a)
+
+-- CheckedMigrations {pendings} = do
+-- forM_ pendings \(PendingMigration (m :: m)) -> do
+--   case cast m of
+--     Just BaseMigration -> Tx.transaction Tx.Serializable Tx.Write $ Tx.sql $ Text.encodeUtf8 $ up @BaseMigration
+--     _ -> pure ()
+--   _now <-
+--     Tx.transaction Tx.Serializable Tx.Write $
+--       Tx.statement
+--         (migrationName @m, up @m, down @m)
+--         [Sql.singletonStatement|
+--       INSERT INTO hasql_mover_migration (name, up, down) VALUES($1::text, $2::text, $3::text)
+--       RETURNING time::timestamptz
+--     |]
+--   pure ()
 
 --------------------------------------------------------------------------------
 -- Declaring migrations
